9a30f6d9eb1b659ca9a37573b7b80629e24cb3b6
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2016-10-20 21:36:16 -0300

    Adding MvcWifClaimsDemo solution (module 11).

---
+++ mod11/MvcWifClaimsDemo/MvcWifClaimsDemo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace MvcWifClaimsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ClaimsIdentity claimsIdentity = System.Web.HttpContext.Current.User.Identity as ClaimsIdentity;
+            ViewBag.Claims = claimsIdentity.Claims;
+
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
