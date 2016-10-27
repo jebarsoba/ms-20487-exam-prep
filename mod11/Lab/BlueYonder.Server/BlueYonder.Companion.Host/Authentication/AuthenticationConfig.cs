533d082a83c0ec00634318ac59e4897bb108ba57
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2016-10-26 21:09:02 -0300

    Lab 11 completed until task 2.7.

---
+++ mod11/Lab/BlueYonder.Server/BlueYonder.Companion.Host/Authentication/AuthenticationConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.WindowsAzure;
+using Thinktecture.IdentityModel.Tokens.Http;
+
+namespace BlueYonder.Companion.Host.Authentication
+{
+    public class AuthenticationConfig
+    {
+        public static AuthenticationConfiguration CreateConfiguration()
+        {
+            AuthenticationConfiguration config = new AuthenticationConfiguration();
+
+            string issuerName = CloudConfigurationManager.GetSetting("ACS.IssuerName").Trim();
+            string realm = CloudConfigurationManager.GetSetting("ACS.Realm").Trim();
+            string signingKey = CloudConfigurationManager.GetSetting("ACS.SigningKey").Trim();
+
+            return config;
+        }
+    }
+}
