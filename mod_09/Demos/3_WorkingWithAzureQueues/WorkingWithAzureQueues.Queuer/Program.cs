3c87adad3f8b6e6e7df3341d3dedde7f0b0241e8
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2016-12-03 15:04:11 -0300

    Demo: Working with queues (module 9).

---
+++ mod_09/Demos/3_WorkingWithAzureQueues/WorkingWithAzureQueues.Queuer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkingWithAzureQueues.Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Sender";
+            string connectionString = ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString;
+            CloudStorageAccount account = CloudStorageAccount.Parse(connectionString);
+            CloudQueueClient client = account.CreateCloudQueueClient();
+            CloudQueue queue = client.GetQueueReference("messagesqueue");
+            queue.CreateIfNotExists();
+
+            Console.WriteLine("Sending messages... Close the console window to stop.\n\n");            
+            for (int i=1; ;i++)
+            {
+                string msg = string.Format("message number {0} on {1}", i, DateTime.Now.ToString());
+                CloudQueueMessage message = new CloudQueueMessage(msg);
+                queue.AddMessage(message);
+                Console.WriteLine(string.Concat("Created ", msg));
+                Thread.Sleep(50);
+            }            
+        }
+    }
+}
---
+++ mod_09/Demos/3_WorkingWithAzureQueues/WorkingWithAzureQueues.Worker/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkingWithAzureQueues.Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Receiver";
+            string connectionString = ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString;
+            CloudStorageAccount account = CloudStorageAccount.Parse(connectionString);
+            CloudQueueClient client = account.CreateCloudQueueClient();
+            CloudQueue queue = client.GetQueueReference("messagesqueue");
+            queue.CreateIfNotExists();            
+
+            Console.WriteLine("Waiting for messages... Close the console window to stop.\n\n");
+            while (true)
+            {
+                CloudQueueMessage message = queue.GetMessage();
+                if (message != null)
+                {
+                    Console.WriteLine("Processing message {0} with string '{1}'", message.Id, message.AsString);
+
+                    // process message
+                    // ...
+
+                    queue.DeleteMessage(message);
+
+                }
+                else
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
