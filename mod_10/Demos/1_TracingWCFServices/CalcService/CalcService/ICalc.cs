8e63dde04a90ee25dc9fe37226212ef6bc180442
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2016-12-03 19:04:03 -0300

    Demo: Tracing WCF Services (module 10).

---
+++ mod_10/Demos/1_TracingWCFServices/CalcService/CalcService/ICalc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CalcService
+{
+    [ServiceContract]
+    public interface ICalc
+    {
+        [OperationContract]
+        int Add(int a, int b);
+        [OperationContract]
+        int Sub(int a, int b);
+        [OperationContract]
+        int Mul(int a, int b);
+        [OperationContract]
+        int Div(int a, int b);
+    }
+}
---
+++ mod_10/Demos/1_TracingWCFServices/CalcService/CalcService/SimpleCalculator.svc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CalcService
+{
+    public class SimpleCalculator : ICalc
+    {
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a*b;
+        }
+
+        public int Div(int a, int b)
+        {
+            return a/b;
+        }
+    }
+}
