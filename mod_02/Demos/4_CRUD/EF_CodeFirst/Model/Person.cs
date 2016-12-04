bc5041eabe6886678574e96b36b6a6c4c3a21663
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2016-12-04 19:30:16 -0300

    Demo: CRUD Operations in Entity Framework (module 2).

---
+++ mod_02/Demos/4_CRUD/EF_CodeFirst/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CodeFirst.Model
+{
+    public class Person
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
---
+++ mod_02/Demos/4_CRUD/EF_CodeFirst/Program.cs
@@ -0,0 +1,31 @@
+﻿using EF_CodeFirst.Infra;
+using EF_CodeFirst.Model;
+using System;
+using System.Linq;
+
+namespace EF_CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initializing the database and populating seed data using DropCreateDatabaseIfModelChanges initializer
+            (new DropCreateDBOnModelChanged()).InitializeDatabase(new SchoolContext());
+
+            SchoolContext schoolContext = new SchoolContext();
+
+            Course course = schoolContext.Courses.FirstOrDefault(l => l.Name == "WCF");
+
+            course.Students.Add(new Student() { Grade = 10, Name = "Jorge" });
+            course.Students.Add(new Student() { Grade = 9, Name = "Juan" });
+
+            course.CourseTeacher.Salary += 1000;
+
+            course.Students.RemoveAll(student => student.Name == "Student_1");
+
+            schoolContext.SaveChanges();
+
+            Console.WriteLine(course.ToString());
+        }
+    }
+}
